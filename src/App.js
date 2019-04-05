692eb1ece72cac9682c8ed9aca420a10a6a195cd
Gatlin <gatlinschultz@gmail.com>
2019-04-05 02:34:39 -0500

    Adding my favorite project so far

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const urlString = "https://api.themoviedb.org/3/search/movie?api_key=0ecf9584908dba19a06191b6d71c95cf&query="
+
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      searchTerm: 'Tron',
+      isLoading: false,
+      error: null
+    }
+  }
+
+  performSearch = (search) => {
+    this.setState({isLoading: true})
+    return fetch(urlString + (search || 'Tron'))
+    .then(response => {
+      this.setState({isLoading: false})
+      return response.json()
+    })
+    .catch(error => {
+      this.setState({error})
+    })
+  }
+
+  componentDidMount(){
+    this.performSearch(this.state.searchTerm)
+    .then(data => {
+      this.setState({'results': data.results})
+      this.refs.searchTermInput.value = this.state.searchTerm
+    });
+  }
+
+  handleChange = (event) => {
+    let value = event.target.value
+    this.performSearch(value)
+    .then(data => {
+      this.setState({'results': data.results})
+    });
+  }
+
   render() {
+
+
+    let results = this.state.results || [];
+
+    const isLoadingJSX = this.state.isLoading ? 
+      <div>Loading...</div> :
+      '';
+
+    const moviesJSX = results.map(movie => {
+      movie.poster_src = "https://image.tmdb.org/t/p/w185" + (movie.poster_path || "/cTF8lpXvAwT1SLH9kVFZZl0AO0D.jpg");
+      return(
+        <div key={movie.id} className="movieList">
+        <img alt={movie.id} src={movie.poster_src}></img>
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+        </div>
+      )
+    });
+
+    // const errorJSX = this.state.error ?
+    // <div>Error occurred, please try again later...</div> :
+    // '';
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          id="inputField"
+          ref="searchTermInput"
+          placeholder="Enter movie name"
+          onChange={this.handleChange}
+        ></input>
+        {isLoadingJSX}
+        {moviesJSX}
       </div>
     );
   }
